d6cdbd6ea7bc24acc48a7834f52ca9cfa714de50
rbednarek2 <rbednarek2@my.wctc.edu>
2019-09-10 23:40:19 -0500

    update

--- Lab 1/Lab1.Program2/Lab1.Program2/Program.cs
+++ Lab 1/Lab1.Program2/Lab1.Program2/Program.cs
@@ -1,21 +1,35 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace Lab1.Program2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string userInput;
+            int inputValue = 0;
+            ArrayList People = new ArrayList();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //inputValue = Convert.ToInt32(userInput);
+            while (inputValue != 3)
+            {
+                Console.WriteLine("Please enter a: " +
+                    "1) Student\n" +
+                    "2) Teacher\n" +
+                    "3) End\n" +
+                    "Input: ");
+                userInput = Console.ReadLine();
+
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+
+            }
         }
     }
 }
