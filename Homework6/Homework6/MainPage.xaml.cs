c667dca6a5275d159b2112b758f7eb4cb4e2edfe
rbednarek2 <rbednarek2@my.wctc.edu>
2019-09-25 19:15:37 -0500

    Initial

---
+++ Homework6/Homework6/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Homework6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            (sender as ListView).SelectedItem = null;
+
+            if (args.SelectedItem != null)
+            {
+                PageDataViewModel pageData = args.SelectedItem as PageDataViewModel;
+                Page page = (Page)Activator.CreateInstance(pageData.Type);
+                await Navigation.PushAsync(page);
+            }
+        }
+    }
+}
+
