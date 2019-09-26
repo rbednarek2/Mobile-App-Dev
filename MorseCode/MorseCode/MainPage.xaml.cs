3e69da4c461f79a707528230be1a7882641c9bd9
rbednarek2 <rbednarek2@my.wctc.edu>
2019-09-25 19:18:23 -0500

    Adding project to GitHub

---
+++ MorseCode/MorseCode/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Dot_Clicked(object sender, System.EventArgs e)
+        {
+            current.Text = current.Text + ".";
+        }
+
+        void Dash_Clicked(object sender, System.EventArgs e)
+        {
+            current.Text = current.Text + "-";
+        }
+
+        void Space_Clicked(object sender, System.EventArgs e)
+        {
+            var input = current.Text.ToString();
+            if (input != "")
+            {
+                current.Text = "";
+                output.Text = output.Text + Morse.MorseCoder(input).ToString();
+            } else
+            {
+                output.Text = output.Text + " ";
+            }
+
+            
+
+        }
+
+        void Clear_Clicked(object sender, System.EventArgs e)
+        {
+            output.Text = "";
+        }
+    }
+
+ }
